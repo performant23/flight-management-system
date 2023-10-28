22824612263ee53a25211299573e0db9250c9b8e
performant23 <rutam.kathale@gmail.com>
2023-10-29 01:11:58 +0530

    sort functionality

--- airline_search_system.py
+++ airline_search_system.py
@@ -8,10 +8,11 @@ import re
 
 app = Flask(__name__)
 app.secret_key = "flight_planner"
-DB_HOST = "localhost"
-DB_NAME = "postgres"
-DB_USER = "postgres"
-DB_PASS = "Rutamkathale55$"
+DB_HOST = "10.2.95.122"
+DB_NAME = "laldinpuia_elvia"
+DB_USER = "laldinpuia_elvia"
+DB_PASS = "1e1c2ec5"
+
 con = psycopg2.connect(database=DB_NAME, user=DB_USER,
                        password=DB_PASS, host=DB_HOST)
 app.permanent_session_lifetime = timedelta(minutes=5)
@@ -25,7 +26,7 @@ def home():
     list_airline_names = [row["Name"] for row in cur.fetchall()]
     return render_template("index.html", list_airline_names=list_airline_names)
 
-
+5
 @app.route("/airlinedirectories", methods=['GET', 'POST'])
 def airlinedir():
     cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -258,9 +259,9 @@ def login():
             session['name'] = user['name']
             session['email'] = user['email']
             message = 'Logged in successfully!'
-            return render_template('user.html', message=message)
+            return render_template('index.html', message=message)
         else:
-            message = 'Please enter correct email / password!'
+            message = flash ('Please enter correct email / password!')
     return render_template('login.html', message=message)
 
 
@@ -271,6 +272,20 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('login'))
 
+@app.route('/profile')
+def profile():
+    # Check if the user is logged in
+    if 'loggedin' in session and session['loggedin']:
+        # Get user information from the session
+        name = session['name']
+        email = session['email']
+        
+        
+        # Render the profile template with user information
+        return render_template('profile.html', name=name, email=email)
+    else:
+        # If user is not logged in, redirect to the login page
+        return redirect('/login')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -298,36 +313,36 @@ def register():
         message = 'Please fill out the form!'
     return render_template('register.html', message=message)
 
-
 @app.route("/search", methods=['GET', 'POST'])
 def search():
-    # Read country names from CSV file
-    with open('Country.csv', 'r') as f:
-        reader = csv.DictReader(f)
-        countries = [row['Name'] for row in reader]
+    cur = con.cursor()
+    cur.execute("SELECT \"Name\" FROM Country")
+    countries = [row[0] for row in cur.fetchall()]
 
     if request.method == 'POST':
         source = request.form.get('source')
         destination = request.form.get('destination')
-        # Store the form data in the session so it can be accessed in the /search/results route
         session['source'] = source
         session['destination'] = destination
-
         return redirect(url_for('search_results'))
-    else:
-        return render_template("searchflights.html", countries=countries)
+
+    # Check if the user is logged in
+    if 'loggedin' not in session:
+        flash('Please login or register first!', 'warning')
+        return redirect(url_for('login'))
+
+    return render_template("searchflights.html", countries=countries)
 
 
-
-@app.route("/search/results", methods=['GET'])
-def search_results():
-    source = session.get('source')
-    destination = session.get('destination')
+@app.route("/search/results", methods=['GET', 'POST'])
+def search_results(source=None, destination=None):
+    if source is None or destination is None:
+        source = session.get('source')
+        destination = session.get('destination')
 
     cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    
     query = """
-    -- Find direct and indirect flights from UK to US
+-- Find direct and indirect flights
 WITH RECURSIVE FlightPaths AS (
   -- Base case: direct flights
   SELECT
@@ -339,15 +354,16 @@ WITH RECURSIVE FlightPaths AS (
     a2."Name" AS "Destination Airport",
     a2."Country" AS "Destination Country",
     a1."Name" || '->' || a2."Name" AS "Path",
-    0 AS "hops"
+    0 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r."Arrival Time" - r."Departure Time")) AS "Flight Duration"
   FROM
     Routes r
     JOIN Airports a1 ON r."Source Airport ID" = a1."Airport ID"
     JOIN Airports a2 ON r."Destination Airport ID" = a2."Airport ID"
   WHERE
-    a1."Country" = 'United Kingdom'
-    AND a2."Country" = 'United States'
-  
+    a1."Country" = %s
+    AND a2."Country" = %s
+    
   UNION ALL
   
   -- Recursive case: indirect flights with one stop
@@ -360,7 +376,9 @@ WITH RECURSIVE FlightPaths AS (
     a3."Name" AS "Destination Airport",
     a3."Country" AS "Destination Country",
     a1."Name" || '->' || a2."Name" || '->' || a3."Name" AS "Path",
-    1 AS "hops"
+    1 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r1."Arrival Time" - r1."Departure Time")) + 
+    ABS(EXTRACT(EPOCH FROM r2."Arrival Time" - r2."Departure Time")) AS "Flight Duration"
   FROM
     Routes r1
     JOIN Airports a1 ON r1."Source Airport ID" = a1."Airport ID"
@@ -368,32 +386,335 @@ WITH RECURSIVE FlightPaths AS (
     JOIN Airports a2 ON r2."Source Airport ID" = a2."Airport ID"
     JOIN Airports a3 ON r2."Destination Airport ID" = a3."Airport ID"
   WHERE
-    a1."Country" = 'United Kingdom'
-    AND a3."Country" = 'United States'
-    AND a2."Country" <> 'United Kingdom'
-    AND a2."Country" <> 'United States'
+    a1."Country" = %s
+    AND a3."Country" = %s
+    AND a2."Country" <> %s
+    AND a2."Country" <> %s
 )
 SELECT
   "Airline",
   "Airline ID",
-  "Price",
   "Source Airport",
   "Source Country",
   "Destination Airport",
   "Destination Country",
   "Path",
-  "hops"
+  "hops",
+  "Flight Duration",
+  "Price"
 FROM
   FlightPaths
 ORDER BY
   "hops";
-    """
-    cur.execute(query, {'source': source, 'destination': destination})
 
+        """
+    cur.execute(query, (source, destination, source, destination, source, destination))
     result = cur.fetchall()
-
     return render_template("searchresults.html", flights=result)
 
+from flask import request, session
+
+@app.route("/sort", methods=['POST'])
+def sort():
+    sort_option = request.form['sort_option']
+    sort_order = request.form['sort_order']
+
+    if 'source' in session and 'destination' in session:
+        source = session['source']
+        destination = session['destination']
+    else:
+        # Handle the case when source and destination are not set in the session
+        return redirect(url_for('search'))
+
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # Define the SQL query based on the selected sorting option and order
+    if sort_option == 'price' and sort_order == 'asc':
+        query = """
+-- Find direct and indirect flights
+WITH RECURSIVE FlightPaths AS (
+  -- Base case: direct flights
+  SELECT
+    r."Airline",
+    r."Airline ID",
+    r."Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a2."Name" AS "Destination Airport",
+    a2."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" AS "Path",
+    0 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r."Arrival Time" - r."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r
+    JOIN Airports a1 ON r."Source Airport ID" = a1."Airport ID"
+    JOIN Airports a2 ON r."Destination Airport ID" = a2."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a2."Country" = %s
+    
+  UNION ALL
+  
+  -- Recursive case: indirect flights with one stop
+  SELECT
+    r1."Airline",
+    r1."Airline ID",
+    r1."Price" + r2."Price" AS "Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a3."Name" AS "Destination Airport",
+    a3."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" || '->' || a3."Name" AS "Path",
+    1 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r1."Arrival Time" - r1."Departure Time")) + 
+    ABS(EXTRACT(EPOCH FROM r2."Arrival Time" - r2."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r1
+    JOIN Airports a1 ON r1."Source Airport ID" = a1."Airport ID"
+    JOIN Routes r2 ON r1."Destination Airport ID" = r2."Source Airport ID"
+    JOIN Airports a2 ON r2."Source Airport ID" = a2."Airport ID"
+    JOIN Airports a3 ON r2."Destination Airport ID" = a3."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a3."Country" = %s
+    AND a2."Country" <> %s
+    AND a2."Country" <> %s
+)
+SELECT
+  "Airline",
+  "Airline ID",
+  "Source Airport",
+  "Source Country",
+  "Destination Airport",
+  "Destination Country",
+  "Path",
+  "hops",
+  "Flight Duration",
+  "Price"
+FROM
+  FlightPaths
+ORDER BY
+  "Price";
+        """
+    elif sort_option == 'price' and sort_order == 'desc':
+        query = """
+-- Find direct and indirect flights
+WITH RECURSIVE FlightPaths AS (
+  -- Base case: direct flights
+  SELECT
+    r."Airline",
+    r."Airline ID",
+    r."Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a2."Name" AS "Destination Airport",
+    a2."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" AS "Path",
+    0 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r."Arrival Time" - r."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r
+    JOIN Airports a1 ON r."Source Airport ID" = a1."Airport ID"
+    JOIN Airports a2 ON r."Destination Airport ID" = a2."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a2."Country" = %s
+    
+  UNION ALL
+  
+  -- Recursive case: indirect flights with one stop
+  SELECT
+    r1."Airline",
+    r1."Airline ID",
+    r1."Price" + r2."Price" AS "Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a3."Name" AS "Destination Airport",
+    a3."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" || '->' || a3."Name" AS "Path",
+    1 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r1."Arrival Time" - r1."Departure Time")) + 
+    ABS(EXTRACT(EPOCH FROM r2."Arrival Time" - r2."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r1
+    JOIN Airports a1 ON r1."Source Airport ID" = a1."Airport ID"
+    JOIN Routes r2 ON r1."Destination Airport ID" = r2."Source Airport ID"
+    JOIN Airports a2 ON r2."Source Airport ID" = a2."Airport ID"
+    JOIN Airports a3 ON r2."Destination Airport ID" = a3."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a3."Country" = %s
+    AND a2."Country" <> %s
+    AND a2."Country" <> %s
+)
+SELECT
+  "Airline",
+  "Airline ID",
+  "Source Airport",
+  "Source Country",
+  "Destination Airport",
+  "Destination Country",
+  "Path",
+  "hops",
+  "Flight Duration",
+  "Price"
+FROM
+  FlightPaths
+ORDER BY
+  "Price" DESC;
+        """
+    elif sort_option == 'duration' and sort_order == 'asc':
+        query = """
+-- Find direct and indirect flights
+WITH RECURSIVE FlightPaths AS (
+  -- Base case: direct flights
+  SELECT
+    r."Airline",
+    r."Airline ID",
+    r."Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a2."Name" AS "Destination Airport",
+    a2."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" AS "Path",
+    0 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r."Arrival Time" - r."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r
+    JOIN Airports a1 ON r."Source Airport ID" = a1."Airport ID"
+    JOIN Airports a2 ON r."Destination Airport ID" = a2."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a2."Country" = %s
+    
+  UNION ALL
+  
+  -- Recursive case: indirect flights with one stop
+  SELECT
+    r1."Airline",
+    r1."Airline ID",
+    r1."Price" + r2."Price" AS "Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a3."Name" AS "Destination Airport",
+    a3."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" || '->' || a3."Name" AS "Path",
+    1 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r1."Arrival Time" - r1."Departure Time")) + 
+    ABS(EXTRACT(EPOCH FROM r2."Arrival Time" - r2."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r1
+    JOIN Airports a1 ON r1."Source Airport ID" = a1."Airport ID"
+    JOIN Routes r2 ON r1."Destination Airport ID" = r2."Source Airport ID"
+    JOIN Airports a2 ON r2."Source Airport ID" = a2."Airport ID"
+    JOIN Airports a3 ON r2."Destination Airport ID" = a3."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a3."Country" = %s
+    AND a2."Country" <> %s
+    AND a2."Country" <> %s
+)
+SELECT
+  "Airline",
+  "Airline ID",
+  "Source Airport",
+  "Source Country",
+  "Destination Airport",
+  "Destination Country",
+  "Path",
+  "hops",
+  "Flight Duration",
+  "Price"
+FROM
+  FlightPaths
+ORDER BY
+  "Flight Duration";
+        """
+    elif sort_option == 'duration' and sort_order == 'desc':
+        query = """
+-- Find direct and indirect flights
+WITH RECURSIVE FlightPaths AS (
+  -- Base case: direct flights
+  SELECT
+    r."Airline",
+    r."Airline ID",
+    r."Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a2."Name" AS "Destination Airport",
+    a2."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" AS "Path",
+    0 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r."Arrival Time" - r."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r
+    JOIN Airports a1 ON r."Source Airport ID" = a1."Airport ID"
+    JOIN Airports a2 ON r."Destination Airport ID" = a2."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a2."Country" = %s
+    
+  UNION ALL
+  
+  -- Recursive case: indirect flights with one stop
+  SELECT
+    r1."Airline",
+    r1."Airline ID",
+    r1."Price" + r2."Price" AS "Price",
+    a1."Name" AS "Source Airport",
+    a1."Country" AS "Source Country",
+    a3."Name" AS "Destination Airport",
+    a3."Country" AS "Destination Country",
+    a1."Name" || '->' || a2."Name" || '->' || a3."Name" AS "Path",
+    1 AS "hops",
+    ABS(EXTRACT(EPOCH FROM r1."Arrival Time" - r1."Departure Time")) + 
+    ABS(EXTRACT(EPOCH FROM r2."Arrival Time" - r2."Departure Time")) AS "Flight Duration"
+  FROM
+    Routes r1
+    JOIN Airports a1 ON r1."Source Airport ID" = a1."Airport ID"
+    JOIN Routes r2 ON r1."Destination Airport ID" = r2."Source Airport ID"
+    JOIN Airports a2 ON r2."Source Airport ID" = a2."Airport ID"
+    JOIN Airports a3 ON r2."Destination Airport ID" = a3."Airport ID"
+  WHERE
+    a1."Country" = %s
+    AND a3."Country" = %s
+    AND a2."Country" <> %s
+    AND a2."Country" <> %s
+)
+SELECT
+  "Airline",
+  "Airline ID",
+  "Source Airport",
+  "Source Country",
+  "Destination Airport",
+  "Destination Country",
+  "Path",
+  "hops",
+  "Flight Duration",
+  "Price"
+FROM
+  FlightPaths
+ORDER BY
+  "Flight Duration" DESC;
+        """
+    else:
+        # Handle invalid sorting option or order
+        flash('Invalid sorting option or order', 'danger')
+        return redirect(url_for('search_results'))
+
+    cur.execute(query, (source, destination, source, destination, source, destination))
+
+    result = cur.fetchall()
+    return render_template("searchresults.html", flights=result)
+
+
+
+@app.before_request
+def before_request():
+    # This function will be called before each request
+    if 'loggedin' not in session and request.endpoint not in ['login', 'register']:
+        flash('Please login or register first!', 'warning')
+        return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)

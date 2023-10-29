1178b9b96557877b8241db3e36abaadfaa8271a3
performant23 <rutam.kathale@gmail.com>
2023-10-29 18:54:44 +0530

    save_to_profile

--- airline_search_system.py
+++ airline_search_system.py
@@ -292,13 +292,118 @@ def profile():
         # Get user information from the session
         name = session['name']
         email = session['email']
+
+        # Check if flight_id is present in the query parameters
+        flight_id = request.args.get('flight_id')
+        flight_price = request.args.get('flight_price')
+
+        if flight_id and flight_price:
+            # Fetch details of the booked flight
+            with con.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                booked_query = """
+
+    WITH RECURSIVE FlightPaths AS (
+      -- Base case: direct flights
+      SELECT
+        r."Airline",
+        r."Airline ID",
+        r."Price",
+        a1."Name" AS "Source Airport",
+        a1."Country" AS "Source Country",
+        a2."Name" AS "Destination Airport",
+        a2."Country" AS "Destination Country",
+        a1."Name" || '->' || a2."Name" AS "Path",
+        0 AS "hops",
+        ABS(EXTRACT(EPOCH FROM r."Arrival Time" - r."Departure Time")) AS "Flight Duration"
+      FROM
+        Routes r
+        JOIN Airports a1 ON r."Source Airport ID" = a1."Airport ID"
+        JOIN Airports a2 ON r."Destination Airport ID" = a2."Airport ID"
+      WHERE
+        a1."Country" = %s
+        AND a2."Country" = %s
+
+      UNION ALL
+
+      -- Recursive case: indirect flights with one stop
+      SELECT
+        r1."Airline",
+        r1."Airline ID",
+        r1."Price" + r2."Price" AS "Price",
+        a1."Name" AS "Source Airport",
+        a1."Country" AS "Source Country",
+        a3."Name" AS "Destination Airport",
+        a3."Country" AS "Destination Country",
+        a1."Name" || '->' || a2."Name" || '->' || a3."Name" AS "Path",
+        1 AS "hops",
+        ABS(EXTRACT(EPOCH FROM r1."Arrival Time" - r1."Departure Time")) + 
+        ABS(EXTRACT(EPOCH FROM r2."Arrival Time" - r2."Departure Time")) AS "Flight Duration"
+      FROM
+        Routes r1
+        JOIN Airports a1 ON r1."Source Airport ID" = a1."Airport ID"
+        JOIN Routes r2 ON r1."Destination Airport ID" = r2."Source Airport ID"
+        JOIN Airports a2 ON r2."Source Airport ID" = a2."Airport ID"
+        JOIN Airports a3 ON r2."Destination Airport ID" = a3."Airport ID"
+      WHERE
+        a1."Country" = %s
+        AND a3."Country" = %s
+        AND a2."Country" <> %s
+        AND a2."Country" <> %s
+    )
+    SELECT
+      "Airline",
+      "Airline ID",
+      "Source Airport",
+      "Source Country",
+      "Destination Airport",
+      "Destination Country",
+      "Path",
+      "hops",
+      "Flight Duration",
+      "Price"
+    FROM
+      FlightPaths
+    WHERE "Airline ID" = %s
+    AND "Price" = %s
+    ORDER BY
+      "hops";
+"""
+
+                cur.execute(booked_query, (session['source'], session['destination'], session['source'], session['destination'], session['source'], session['destination'], flight_id, flight_price))
+
+                booked_flight = cur.fetchone()
+
+            with con.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                insert_query = """
+    INSERT INTO BookedFlight ("email", "airline", "airlineid", "sourceairport", "sourcecountry", 
+                               "destinationairport", "destinationcountry", "path", "hops", 
+                               "flightduration", "price")
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                RETURNING *;
+            """
+                cur.execute(insert_query, (email, booked_flight["Airline"], booked_flight["Airline ID"],
+                                      booked_flight["Source Airport"], booked_flight["Source Country"],
+                                      booked_flight["Destination Airport"], booked_flight["Destination Country"],
+                                      booked_flight["Path"], booked_flight["hops"],
+                                      booked_flight["Flight Duration"], booked_flight["Price"]))
+                con.commit()
+
+            # Fetch the inserted record
+                inserted_record = cur.fetchone()
         
+
+            # Render the profile template with user information and booked flight details
+            return render_template('profile.html', name=name, email=email, booked_flight=booked_flight)
+        else:
+            # If flight_id is not provided, render the profile template with only user information
+            return render_template('profile.html', name=name, email=email)
         
-        # Render the profile template with user information
-        return render_template('profile.html', name=name, email=email)
+
     else:
         # If user is not logged in, redirect to the login page
         return redirect('/login')
+    
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
